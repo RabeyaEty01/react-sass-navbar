89e24ef8c885187a8e7438289c9767f2ae0b05ca
RabeyaEty01 <etyrabeya01@gmail.com>
2021-11-24 23:18:47 +0600

    first commit

---
+++ src/Pages/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../assests/Styles/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+         <div className="navbar-content">
+         <img src="" alt="" />
+         </div>
+         <div className="navbar-content-links">
+             <a href="#">About Us</a>
+             <a href="#">Services</a>
+             <a href="#">Training School</a>
+             <a href="#">Our Clients</a>
+             <a href="#">Careers</a>
+             <a href="#">News & Events</a>
+           
+         </div>
+        </div>
+    );
+};
+
+export default Navbar;
